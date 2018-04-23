614927d24e2779e69f0e163e971d896483d61d73
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/LinqToSqlMasterDetail/Form1.vb
@@ -0,0 +1,27 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Forms
+Imports DevExpress.XtraReports.UI
+
+Namespace LinqToSqlMasterDetail
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+            Dim dc As New NwindDataContext()
+            Dim report As New XtraReport1()
+            Dim bs As BindingSource = CType(report.DataSource, BindingSource)
+            bs.DataSource = dc.Orders
+            report.ShowPreviewDialog()
+        End Sub
+    End Class
+End Namespace
---
+++ VB/LinqToSqlMasterDetail/XtraReport1.Designer.vb
@@ -0,0 +1,208 @@
+﻿Namespace LinqToSqlMasterDetail
+    Partial Public Class XtraReport1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary> 
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+            Me.xrLabel7 = New DevExpress.XtraReports.UI.XRLabel()
+            Me.xrLabel6 = New DevExpress.XtraReports.UI.XRLabel()
+            Me.xrLabel5 = New DevExpress.XtraReports.UI.XRLabel()
+            Me.PageHeader = New DevExpress.XtraReports.UI.PageHeaderBand()
+            Me.PageFooter = New DevExpress.XtraReports.UI.PageFooterBand()
+            Me.masterBS = New System.Windows.Forms.BindingSource(Me.components)
+            Me.DetailReport = New DevExpress.XtraReports.UI.DetailReportBand()
+            Me.Detail1 = New DevExpress.XtraReports.UI.DetailBand()
+            Me.xrLabel4 = New DevExpress.XtraReports.UI.XRLabel()
+            Me.xrLabel3 = New DevExpress.XtraReports.UI.XRLabel()
+            Me.xrLabel2 = New DevExpress.XtraReports.UI.XRLabel()
+            Me.xrLabel1 = New DevExpress.XtraReports.UI.XRLabel()
+            Me.topMarginBand1 = New DevExpress.XtraReports.UI.TopMarginBand()
+            Me.bottomMarginBand1 = New DevExpress.XtraReports.UI.BottomMarginBand()
+            DirectCast(Me.masterBS, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+            ' 
+            ' Detail
+            ' 
+            Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrLabel7, Me.xrLabel6, Me.xrLabel5})
+            Me.Detail.Dpi = 100F
+            Me.Detail.HeightF = 25F
+            Me.Detail.Name = "Detail"
+            Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+            Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+            ' 
+            ' xrLabel7
+            ' 
+            Me.xrLabel7.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "OrderDate")})
+            Me.xrLabel7.Dpi = 100F
+            Me.xrLabel7.LocationFloat = New DevExpress.Utils.PointFloat(417F, 0F)
+            Me.xrLabel7.Name = "xrLabel7"
+            Me.xrLabel7.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+            Me.xrLabel7.SizeF = New System.Drawing.SizeF(225F, 25F)
+            Me.xrLabel7.Text = "xrLabel7"
+            ' 
+            ' xrLabel6
+            ' 
+            Me.xrLabel6.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "OrderID")})
+            Me.xrLabel6.Dpi = 100F
+            Me.xrLabel6.LocationFloat = New DevExpress.Utils.PointFloat(117F, 0F)
+            Me.xrLabel6.Name = "xrLabel6"
+            Me.xrLabel6.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+            Me.xrLabel6.SizeF = New System.Drawing.SizeF(100F, 25F)
+            Me.xrLabel6.Text = "xrLabel6"
+            ' 
+            ' xrLabel5
+            ' 
+            Me.xrLabel5.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "CustomerID")})
+            Me.xrLabel5.Dpi = 100F
+            Me.xrLabel5.LocationFloat = New DevExpress.Utils.PointFloat(8F, 0F)
+            Me.xrLabel5.Name = "xrLabel5"
+            Me.xrLabel5.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+            Me.xrLabel5.SizeF = New System.Drawing.SizeF(100F, 25F)
+            Me.xrLabel5.Text = "xrLabel5"
+            ' 
+            ' PageHeader
+            ' 
+            Me.PageHeader.Dpi = 100F
+            Me.PageHeader.HeightF = 30F
+            Me.PageHeader.Name = "PageHeader"
+            Me.PageHeader.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+            Me.PageHeader.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+            ' 
+            ' PageFooter
+            ' 
+            Me.PageFooter.Dpi = 100F
+            Me.PageFooter.HeightF = 30F
+            Me.PageFooter.Name = "PageFooter"
+            Me.PageFooter.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+            Me.PageFooter.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+            ' 
+            ' masterBS
+            ' 
+            Me.masterBS.DataSource = GetType(LinqToSqlMasterDetail.Order)
+            ' 
+            ' DetailReport
+            ' 
+            Me.DetailReport.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail1})
+            Me.DetailReport.DataMember = "Order_Details"
+            Me.DetailReport.DataSource = Me.masterBS
+            Me.DetailReport.Dpi = 100F
+            Me.DetailReport.Level = 0
+            Me.DetailReport.Name = "DetailReport"
+            ' 
+            ' Detail1
+            ' 
+            Me.Detail1.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrLabel4, Me.xrLabel3, Me.xrLabel2, Me.xrLabel1})
+            Me.Detail1.Dpi = 100F
+            Me.Detail1.HeightF = 25F
+            Me.Detail1.Name = "Detail1"
+            ' 
+            ' xrLabel4
+            ' 
+            Me.xrLabel4.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Order_Details.UnitPrice")})
+            Me.xrLabel4.Dpi = 100F
+            Me.xrLabel4.Font = New System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, (CByte(204)))
+            Me.xrLabel4.LocationFloat = New DevExpress.Utils.PointFloat(542F, 0F)
+            Me.xrLabel4.Name = "xrLabel4"
+            Me.xrLabel4.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+            Me.xrLabel4.SizeF = New System.Drawing.SizeF(100F, 25F)
+            Me.xrLabel4.StylePriority.UseFont = False
+            Me.xrLabel4.Text = "xrLabel4"
+            ' 
+            ' xrLabel3
+            ' 
+            Me.xrLabel3.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Order_Details.Quantity")})
+            Me.xrLabel3.Dpi = 100F
+            Me.xrLabel3.Font = New System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, (CByte(204)))
+            Me.xrLabel3.LocationFloat = New DevExpress.Utils.PointFloat(417F, 0F)
+            Me.xrLabel3.Name = "xrLabel3"
+            Me.xrLabel3.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+            Me.xrLabel3.SizeF = New System.Drawing.SizeF(100F, 25F)
+            Me.xrLabel3.StylePriority.UseFont = False
+            Me.xrLabel3.Text = "xrLabel3"
+            ' 
+            ' xrLabel2
+            ' 
+            Me.xrLabel2.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Order_Details.ProductID")})
+            Me.xrLabel2.Dpi = 100F
+            Me.xrLabel2.Font = New System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, (CByte(204)))
+            Me.xrLabel2.LocationFloat = New DevExpress.Utils.PointFloat(292F, 0F)
+            Me.xrLabel2.Name = "xrLabel2"
+            Me.xrLabel2.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+            Me.xrLabel2.SizeF = New System.Drawing.SizeF(100F, 25F)
+            Me.xrLabel2.StylePriority.UseFont = False
+            Me.xrLabel2.Text = "xrLabel2"
+            ' 
+            ' xrLabel1
+            ' 
+            Me.xrLabel1.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Order_Details.OrderID")})
+            Me.xrLabel1.Dpi = 100F
+            Me.xrLabel1.Font = New System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, (CByte(204)))
+            Me.xrLabel1.LocationFloat = New DevExpress.Utils.PointFloat(167F, 0F)
+            Me.xrLabel1.Name = "xrLabel1"
+            Me.xrLabel1.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+            Me.xrLabel1.SizeF = New System.Drawing.SizeF(100F, 25F)
+            Me.xrLabel1.StylePriority.UseFont = False
+            Me.xrLabel1.Text = "xrLabel1"
+            ' 
+            ' topMarginBand1
+            ' 
+            Me.topMarginBand1.Dpi = 100F
+            Me.topMarginBand1.HeightF = 100F
+            Me.topMarginBand1.Name = "topMarginBand1"
+            ' 
+            ' bottomMarginBand1
+            ' 
+            Me.bottomMarginBand1.Dpi = 100F
+            Me.bottomMarginBand1.HeightF = 100F
+            Me.bottomMarginBand1.Name = "bottomMarginBand1"
+            ' 
+            ' XtraReport1
+            ' 
+            Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail, Me.PageHeader, Me.PageFooter, Me.DetailReport, Me.topMarginBand1, Me.bottomMarginBand1})
+            Me.DataSource = Me.masterBS
+            Me.Version = "16.2"
+            DirectCast(Me.masterBS, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+        End Sub
+
+        #End Region
+
+        Private Detail As DevExpress.XtraReports.UI.DetailBand
+        Private PageHeader As DevExpress.XtraReports.UI.PageHeaderBand
+        Private PageFooter As DevExpress.XtraReports.UI.PageFooterBand
+        Private xrLabel7 As DevExpress.XtraReports.UI.XRLabel
+        Private xrLabel6 As DevExpress.XtraReports.UI.XRLabel
+        Private xrLabel5 As DevExpress.XtraReports.UI.XRLabel
+        Private DetailReport As DevExpress.XtraReports.UI.DetailReportBand
+        Private Detail1 As DevExpress.XtraReports.UI.DetailBand
+        Private xrLabel4 As DevExpress.XtraReports.UI.XRLabel
+        Private xrLabel3 As DevExpress.XtraReports.UI.XRLabel
+        Private xrLabel2 As DevExpress.XtraReports.UI.XRLabel
+        Private xrLabel1 As DevExpress.XtraReports.UI.XRLabel
+        Private masterBS As System.Windows.Forms.BindingSource
+        Private topMarginBand1 As DevExpress.XtraReports.UI.TopMarginBand
+        Private bottomMarginBand1 As DevExpress.XtraReports.UI.BottomMarginBand
+    End Class
+End Namespace
